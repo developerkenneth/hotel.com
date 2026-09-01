a6e2329d8cb34af59e49e9d1586ce403f8b6e920
Onyeka Chisom <splashanthony5@gmail.com>
2026-09-01 12:09:18 +0100

    I updated the room details: I added a loading animation before the room details show

--- ui/index.php
+++ ui/index.php
@@ -11,6 +11,10 @@
 
 <body>
     <div class="body">
+        <div class="box">
+       
+     </div>
+    <div class="main-content hide">
         <div class="header">
             <h1 class="head">Deluxe Rooms</h1>
             <p class="ppp"><span class="icon"><i class="fa-solid fa-star"></i></span>4.8 rated</p>
@@ -30,153 +34,153 @@
         </div>
         <div class="next-section">
 
-    <!-- PRICE -->
-    <div class="price">
-        <h1 class="bott">Price</h1>
+            <!-- PRICE -->
+            <div class="price">
+                <h1 class="bott">Price</h1>
 
-        <div class="price-info">
-            <h2>₦120,000 <span>/ Night</span></h2>
+                <div class="price-info">
+                    <h2>₦120,000 <span>/ Night</span></h2>
 
-            <p>
-                Enjoy a luxurious and comfortable stay in our Deluxe Room,
-                designed to give you the perfect combination of comfort,
-                relaxation and modern style.
-            </p>
+                    <p>
+                        Enjoy a luxurious and comfortable stay in our Deluxe Room,
+                        designed to give you the perfect combination of comfort,
+                        relaxation and modern style.
+                    </p>
 
-            <p class="small-text">Taxes and additional charges may apply.</p>
-        </div>
-    </div>
-
-
-    <!-- AMENITIES -->
-    <div class="price">
-
-        <h1 class="bott">Amenities</h1>
-
-        <div class="amenities-list">
-
-            <p class="ameni-p">
-                <i class="fa-solid fa-wifi icon"></i>
-                Wifi
-            </p>
-
-            <p class="ameni-p">
-                <i class="fa-solid fa-seedling icon"></i>
-                Garden View
-            </p>
-
-            <p class="ameni-p">
-                <i class="fa-solid fa-tv icon"></i>
-                Smart TV
-            </p>
-
-            <p class="ameni-p">
-                <i class="fa-solid fa-bell icon"></i>
-                Alarm Clock
-            </p>
-
-            <p class="ameni-p">
-                <i class="fa-solid fa-volume-high icon"></i>
-                Sound System
-            </p>
-
-            <p class="ameni-p">
-                <i class="fa-solid fa-phone icon"></i>
-                Telephone
-            </p>
-
-            <p class="ameni-p">
-                <i class="fa-brands fa-playstation icon"></i>
-                PlayStation
-            </p>
-
-        </div>
-
-    </div>
-
-
-    <!-- CAPACITY -->
-    <div class="price">
-
-        <h1 class="bott">Capacity</h1>
-
-        <div class="capacity-info">
-
-            <div class="capacity-item">
-                <i class="fa-solid fa-user-group icon"></i>
-
-                <div>
-                    <h3>4 Guests</h3>
-                    <p>Maximum occupancy</p>
+                    <p class="small-text">Taxes and additional charges may apply.</p>
                 </div>
             </div>
 
 
-            <div class="capacity-item">
-                <i class="fa-solid fa-bed icon"></i>
+            <!-- AMENITIES -->
+            <div class="price">
 
-                <div>
-                    <h3>2 Beds</h3>
-                    <p>Comfortable sleeping space</p>
+                <h1 class="bott">Amenities</h1>
+
+                <div class="amenities-list">
+
+                    <p class="ameni-p">
+                        <i class="fa-solid fa-wifi icon"></i>
+                        Wifi
+                    </p>
+
+                    <p class="ameni-p">
+                        <i class="fa-solid fa-seedling icon"></i>
+                        Garden View
+                    </p>
+
+                    <p class="ameni-p">
+                        <i class="fa-solid fa-tv icon"></i>
+                        Smart TV
+                    </p>
+
+                    <p class="ameni-p">
+                        <i class="fa-solid fa-bell icon"></i>
+                        Alarm Clock
+                    </p>
+
+                    <p class="ameni-p">
+                        <i class="fa-solid fa-volume-high icon"></i>
+                        Sound System
+                    </p>
+
+                    <p class="ameni-p">
+                        <i class="fa-solid fa-phone icon"></i>
+                        Telephone
+                    </p>
+
+                    <p class="ameni-p">
+                        <i class="fa-brands fa-playstation icon"></i>
+                        PlayStation
+                    </p>
+
                 </div>
+
             </div>
 
 
-            <div class="capacity-item">
-                <i class="fa-solid fa-ruler-combined icon"></i>
+            <!-- CAPACITY -->
+            <div class="price">
 
-                <div>
-                    <h3>35 m²</h3>
-                    <p>Room size</p>
+                <h1 class="bott">Capacity</h1>
+
+                <div class="capacity-info">
+
+                    <div class="capacity-item">
+                        <i class="fa-solid fa-user-group icon"></i>
+
+                        <div>
+                            <h3>4 Guests</h3>
+                            <p>Maximum occupancy</p>
+                        </div>
+                    </div>
+
+
+                    <div class="capacity-item">
+                        <i class="fa-solid fa-bed icon"></i>
+
+                        <div>
+                            <h3>2 Beds</h3>
+                            <p>Comfortable sleeping space</p>
+                        </div>
+                    </div>
+
+
+                    <div class="capacity-item">
+                        <i class="fa-solid fa-ruler-combined icon"></i>
+
+                        <div>
+                            <h3>35 m²</h3>
+                            <p>Room size</p>
+                        </div>
+                    </div>
+
                 </div>
+
+            </div>
+
+
+            <!-- AVAILABLE ROOMS -->
+            <div class="price">
+
+                <h1 class="bott">Available Rooms</h1>
+
+                <div class="available-room">
+
+                    <div>
+                        <h3>Deluxe Room</h3>
+                        <p>King Bed • Garden View</p>
+                    </div>
+
+                    <div class="room-price">
+                        <h3>₦120,000</h3>
+                        <span>per night</span>
+                    </div>
+
+                    <button class="book-btn">Book Now</button>
+
+                </div>
+
+
+                <div class="available-room">
+
+                    <div>
+                        <h3>Executive Suite</h3>
+                        <p>2 Beds • Balcony • Smart TV</p>
+                    </div>
+
+                    <div class="room-price">
+                        <h3>₦180,000</h3>
+                        <span>per night</span>
+                    </div>
+
+                    <button class="book-btn">Book Now</button>
+
+                </div>
+
             </div>
 
         </div>
-
-    </div>
-
-
-    <!-- AVAILABLE ROOMS -->
-    <div class="price">
-
-        <h1 class="bott">Available Rooms</h1>
-
-        <div class="available-room">
-
-            <div>
-                <h3>Deluxe Room</h3>
-                <p>King Bed • Garden View</p>
-            </div>
-
-            <div class="room-price">
-                <h3>₦120,000</h3>
-                <span>per night</span>
-            </div>
-
-            <button class="book-btn">Book Now</button>
-
-        </div>
-
-
-        <div class="available-room">
-
-            <div>
-                <h3>Executive Suite</h3>
-                <p>2 Beds • Balcony • Smart TV</p>
-            </div>
-
-            <div class="room-price">
-                <h3>₦180,000</h3>
-                <span>per night</span>
-            </div>
-
-            <button class="book-btn">Book Now</button>
-
-        </div>
-
-    </div>
-
-</div>
     </div>
     <script>
         function changeImage(image) {
@@ -184,6 +188,13 @@
             document.getElementById("mainImage").src = image.src;
 
         }
+        const box = document.querySelector(".box");
+        const maincontent = document.querySelector(".main-content");
+
+        setTimeout(function() {
+            box.classList.add("hide");
+            maincontent.classList.remove("hide");
+        }, 6000)
     </script>
 
 </body>
